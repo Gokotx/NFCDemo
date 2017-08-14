099c336b82e9f529c788b196731f76b62d075448
Goko <chengk@gegejia.com>
2017-08-14 09:39:39 +0800

    Initial Commit

---
+++ NFCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NFCDemo
+//
+//  Created by Goko on 14/08/2017.
+//  Copyright © 2017 Goko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NFCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NFCDemo
+//
+//  Created by Goko on 14/08/2017.
+//  Copyright © 2017 Goko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
