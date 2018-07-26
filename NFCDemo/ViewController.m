bd58a7ca6893747a8a0db2b53cd8ac3ba1e9b290
Goko <gokotx@outlook.com>
2018-07-26 19:23:37 +0800

    NFCDemo
    NFCDemo initial

--- NFCDemo/ViewController.m
+++ NFCDemo/ViewController.m
@@ -6,9 +6,12 @@
 //  Copyright © 2017 Goko. All rights reserved.
 //
 
+#import <CoreNFC/CoreNFC.h>
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<NFCNDEFReaderSessionDelegate>
+
+@property(nonatomic,strong)NFCNDEFReaderSession * nfcSession;
 
 @end
 
@@ -17,8 +20,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.nfcSession = [[NFCNDEFReaderSession alloc] initWithDelegate:self queue:dispatch_queue_create(NULL, DISPATCH_QUEUE_CONCURRENT) invalidateAfterFirstRead:NO];
+    [self.nfcSession beginSession];
+    
 }
-
+-(void)readerSession:(NFCNDEFReaderSession *)session didDetectNDEFs:(NSArray<NFCNDEFMessage *> *)messages{
+    [messages enumerateObjectsUsingBlock:^(NFCNDEFMessage * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [obj.records enumerateObjectsUsingBlock:^(NFCNDEFPayload * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSLog(@"typeNameFormat:%d",obj.typeNameFormat);
+            NSLog(@"payload:%@",obj.payload);
+            NSLog(@"type:%@",obj.type);
+            NSLog(@"identifier:%@",obj.identifier);
+        }];
+    }];
+}
+-(void)readerSession:(NFCNDEFReaderSession *)session didInvalidateWithError:(nonnull NSError *)error{
+    NSLog(@"error:%@",error.localizedDescription);
+}
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    self.nfcSession = [[NFCNDEFReaderSession alloc] initWithDelegate:self queue:dispatch_queue_create(NULL, DISPATCH_QUEUE_CONCURRENT) invalidateAfterFirstRead:YES];
+    [self.nfcSession beginSession];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
